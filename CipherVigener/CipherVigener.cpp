0d46c111a9fca3d464d3b02bff1cc3f2e7362d87
Maxim <maksimrybak666@gmail.com>
2017-07-09 12:16:01 +0300

    key finish

--- CipherVigener/CipherVigener.cpp
+++ CipherVigener/CipherVigener.cpp
@@ -2,10 +2,46 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <stdlib.h>
+#include <string>
 
+using namespace std;
 
-int main()
-{
+string alphabet = {"abcdefghijklmnopqrstuvwxyz"};
+
+string Key(string cipherWord, string keyInput) {
+	int i = 0;
+	string key;
+	for (int i = 0; i < cipherWord.length(); i++) {
+		cout << "len cipher loop: " << cipherWord.length() - keyInput.length();
+		for (int j = 0; j < keyInput.length(); j++) {
+			cout << " key len: " << key.length() << " cipher len: " << cipherWord.length() << endl;
+			if (key.length() == cipherWord.length()) {
+				cout << "yea" << endl;
+				break;
+			}
+			
+			key = key + keyInput[j];
+			cout << " Key : " << key<<endl;
+
+			
+			/*cout << "len: " << cipherWord.length() << endl;
+			cout << "Key interation:" << i << endl;*/
+
+		}
+	}
+	return key;
+};
+
+int main(){
+	string cipherWord;
+	string keyInput;
+	getline(cin, cipherWord);
+	getline(cin, keyInput);
+
+	cout << Key(cipherWord, keyInput);
+	system("pause");
     return 0;
 }
 
